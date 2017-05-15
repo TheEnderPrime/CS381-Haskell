b7b26e4d778bd5ec482205a026eb1b0c59b70d50
ozarowib <ozarowib@users.noreply.github.com>
2017-05-15 13:45:54 -0700

    finished

--- hw3.hs
+++ hw3.hs
@@ -52,3 +52,62 @@ semCmd (POP i) xs = drop i xs
 -}
 
 -- Exercise 2A
+data Shape = X
+            | TD Shape Shape
+            | LR Shape Shape
+            deriving Show
+
+type BBox = (Int, Int)
+
+bbox :: Shape -> BBox
+bbox (X) = (1, 1)
+bbox (TD i j) =
+	let (x1, y1) = bbox i
+	    (x2, y2) = bbox j
+	in (max x1 x2, y1+y2)
+bbox (LR i j) =
+	let (x1, y1) = bbox i
+	    (x2, y2) = bbox j
+	in (x1+x2, max y1 y2)
+
+
+-- Exercise 2B
+rect :: Shape -> Maybe BBox
+rect (X) = Just (1, 1)
+rect (TD i j) = case (rect i, rect j) of
+				(Just (x1, y1), Just (x2, y2)) -> if (x1==x2) then (Just (x1, y1+y2)) else Nothing
+				_ -> Nothing
+rect (LR i j) = case (rect i, rect j) of
+				(Just (x1, y1), Just (x2, y2)) -> if (y1==y2) then (Just (x1+x2, y1)) else Nothing
+				_ -> Nothing
+
+-- Exercise 3A
+{-
+f x y = if null x then [y] else x
+g x y = if not (null x) then [] else [y]
+
+-- 1)
+f :: [a] -> a -> [a]
+g :: [a] -> b -> [b]
+
+-- 2)
+f: Null takes a List so x must be [a]. The types of x and y must be the same to check the conditional.
+g: Same as f, but now returns a different list and the type of y is not related to x anymore.
+
+-- 3)
+g is more general because for f the lists must be the same type.
+
+-- 4)
+f and g are different types because in f the list type is constrained, but not in g.
+-}
+
+-- Exercise 3B
+h :: [b] -> [(a, b)] -> [b]
+h param1 [(param2, param3)] = param3:param1
+
+-- Exercise 3C
+k :: (a -> b) -> ((a -> b) -> a) -> b
+k param1 param2 = (param1 (param2 param1))
+
+-- Exercise 3D
+-- a->b is difficult because there is not an apparent way to take in type a and directly return type b.
