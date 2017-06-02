74bc300e49c60478f0214913120d054fe012322f
ozarowib <ozarowib@users.noreply.github.com>
2017-06-02 13:03:03 -0700

    Part 1 done

--- hw5.pl
+++ hw5.pl
@@ -1,3 +1,4 @@
+/* CS 381 HW 5 - Glenn Upthagrove, Brian Ozarowicz, David Baugh */
 
 /* Exercise 1 */
 
@@ -19,4 +20,14 @@ enroll(mary,261).
 enroll(john,381).
 enroll(jim,399).
 
+schedule(Student,Place,Time) :- enroll(Student,Class), where(Class,Place), when(Class,Time).
+
+usage(Place,Time) :- where(Class,Place), when(Class,Time).
+
+conflict(Class1,Class2) :- where(Class1,Place), where(Class2,Place), when(Class1,Time), when(Class2,Time), Class1\=Class2.
+
+meet(Student1,Student2) :- enroll(Student1,Class), enroll(Student2,Class), Student1\=Student2.
+meet(Student1,Student2) :- enroll(Student1,Class1), enroll(Student2,Class2), where(Class1,Place), where(Class2,Place), when(Class1,Time1), when(Class2,Time2), Time2 is Time1+1, Student1\=Student2.
+
+/* Exercise 2 */
 
